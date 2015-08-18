0311feb326aafeec5ef4762c195005dffaa8db1f
Mina S. Aziz <minaz922@gmail.com>
2015-08-18 16:45:45 +0200

    initialize project

---
+++ facebook_api_demo.js
@@ -0,0 +1,50 @@
+Posts = new Mongo.Collection("posts");
+
+if (Meteor.isClient) {
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '724469854348779',
+      status     : true,
+      xfbml      : true,
+      version    : '2.4'
+    });
+  };
+
+  Template.body.events({
+    'click .pull-data': function () {
+      FB.api(
+        '/verge/feed',
+        'GET',
+        {"fields":"comments,message", "access_token": "724469854348779|DOQ-d6jJe5qXpvjGgh-P5yR-eX0"},
+        function(response) {
+            // Insert your code here
+            for (var i = 0; i < response.data.length; i++) {
+              Posts.insert({text: response.data[i]});
+            }
+        }
+      );
+    }
+  });
+
+  Template.body.helpers({
+    posts: function() {
+      return Posts.find();
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+
+// Meteor.methods({
+//   pullData: function(){
+//     FB.ui(
+//     {
+//       method: 'share',
+//       href: 'https://developers.facebook.com/docs/'
+//     }, function(response){});
+//   }
+// })
